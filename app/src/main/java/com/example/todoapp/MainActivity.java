1be1d0965ce5afaf2de0cab2526da0da1776c163
Julia <julia_zg@wp.pl>
2022-03-20 23:31:13 +0100

    Dodawanie tasków

--- app/src/main/java/com/example/todoapp/MainActivity.java
+++ app/src/main/java/com/example/todoapp/MainActivity.java
@@ -1,15 +1,77 @@
 package com.example.todoapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final int ADD_TASK_REQUEST = 1;
+
+    private TaskViewModel taskViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        FloatingActionButton buttonAddTask = findViewById(R.id.button_add_task);
+        buttonAddTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AddTaskActivity.class);
+                startActivityForResult(intent,ADD_TASK_REQUEST);
+
+            }
+        });
+
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+
+        final TaskAdapter adapter = new TaskAdapter();
+        recyclerView.setAdapter(adapter);
+
+        taskViewModel = new ViewModelProvider(this).get(TaskViewModel.class);
+        taskViewModel.getAllTasks().observe(this, new Observer<List<Task>>() {
+            @Override
+            public void onChanged(List<Task> tasks) {
+                //update RecyclerView
+                adapter.setTasks(tasks);
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == ADD_TASK_REQUEST && resultCode == RESULT_OK)
+        {
+            String title = data.getStringExtra(AddTaskActivity.EXTRA_TITLE);
+            String description = data.getStringExtra(AddTaskActivity.EXTRA_DESCRIPTION);
+            int priority = data.getIntExtra(AddTaskActivity.EXTRA_PRIORITY, 1);
+
+            Task task = new Task(title, description, priority);
+            taskViewModel.insert(task);
+
+            Toast.makeText(this, "Zapisano zadanie", Toast.LENGTH_SHORT).show();
+        } else
+        {
+            Toast.makeText(this, "Nie zapisano zadania", Toast.LENGTH_SHORT).show();
+        }
     }
 }
